85ae7750297cb3586adef97c8780f7a1b260eb35
Sriveer Neerukonda <sriveer@proton.me>
2023-01-18 12:48:07 +0530

    fix:resizing to 1200px or above automatically closes the main nav menu if its open

--- src/main.js
+++ src/main.js
@@ -1,13 +1,30 @@
+let menuOpen = document.getElementById("hamburger-checkbox");
+
 function toggleNavMenu(){
-    if(menuOpen.checked === true){
-        menuOpen.checked = false;
-        document.body.style.overflowY = "auto";
-    }
-    else{
-        menuOpen.checked = true;
-        document.body.style.overflowY = "hidden";
-    }
+    isMenuOpen()?closeMenu():openMenu();
 }
 
-let menuOpen = document.getElementById("hamburger-checkbox");
+function closeMenu(){
+    menuOpen.checked = false;
+    document.body.style.overflowY = "auto";
+}
+function openMenu(){
+    menuOpen.checked = true;
+    document.body.style.overflowY = "hidden";
+}
+function isMenuOpen(){
+    return menuOpen.checked;
+}
+
 document.getElementById("blinder").addEventListener("click",toggleNavMenu);
+menuOpen.addEventListener("input",(e) => {
+    if(e.target.checked === true)
+        openMenu();
+    else closeMenu();
+});
+
+window.addEventListener("resize",function(){
+    console.log("resized");
+    if(document.body.clientWidth >= 1200 && isMenuOpen())
+        closeMenu();
+})
